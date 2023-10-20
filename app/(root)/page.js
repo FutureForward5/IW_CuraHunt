228091abf66df061c20c0be36a82f7c774c3011b
Lakshay goyal <akshaybuster5280@gmail.com>
2023-10-20 19:48:40 +0530

    Update page.js

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -3,7 +3,7 @@ import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/containers/footer/Footer";
 import Header from "@/containers/header/Header";
 import Toplist from "@/containers/Toplist/Toplist";
-
+ 
 export default function Home() {
   return (
     <main>
